e631ca6719aacf001867c54e1a5ab300e74a1b45
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-16 16:00:32 +0200

    refactor(watch): Improve formatting for better readability

--- WhatchApp.Web/Models/Watch.cs
+++ WhatchApp.Web/Models/Watch.cs
@@ -7,14 +7,15 @@ public class Watch
     public int Id { get; set; }
     [Required (ErrorMessage = "You must specify a name")]
     public string Name { get; set; }
+
     [Required(ErrorMessage = "You must specify a description")]
     public string Description { get; set; }
+
     [Required(ErrorMessage = "You must specify a price")]
     [DisplayFormat(DataFormatString = "{0:N2} €")]
     public decimal Price { get; set; }
 
     [Display(Name = "Ref")]
-    
     [Required(ErrorMessage = "You must specify a Reference number")]
     public string ReferenceNumber { get; set; }
 
