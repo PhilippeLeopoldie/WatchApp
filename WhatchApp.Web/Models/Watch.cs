ec47959e7731fc0aac3e3f96ee19435400953f0d
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-16 11:57:19 +0200

    feat(model): Implement Watch class with data annotations

---
+++ WhatchApp.Web/Models/Watch.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WhatchApp.Web.Models;
+
+public class Watch
+{
+    public int Id { get; set; }
+    [Required (ErrorMessage = "You must specify a name")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "You must specify a description")]
+    public string Description { get; set; }
+    [Required(ErrorMessage = "You must specify a price")]
+    [DisplayFormat(DataFormatString = "{0:N2} €")]
+    public decimal Price { get; set; }
+
+    [Display(Name = "Ref")]
+    
+    [Required(ErrorMessage = "You must specify a Reference number")]
+    public string ReferenceNumber { get; set; }
+
+}
