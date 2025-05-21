9413e3bb6f98c945a0c5a5f55d6d5ef99336b631
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-21 23:22:10 +0200

    Add layout and refactor view files for watches
    - Created `_Layout.cshtml` for a common layout with navigation.
    - Updated `Create.cshtml`, `Details.cshtml`, and `Index.cshtml` to use `ViewBag.Title` and removed previous layout structures.
    - Changed `Index.cshtml` to use `IndexVM` for model binding and updated the watch display logic.
    - Added `IndexVM.cs` to define a view model for watches.
    - Updated `_ViewImports.cshtml` with necessary tag helpers and namespaces.
    - Created `_ViewStart.cshtml` to set the layout for views.

---
+++ WhatchApp.Web/Views/Watches/IndexVM.cs
@@ -0,0 +1,14 @@
+﻿namespace WhatchApp.Web.Views.Watches;
+
+public class IndexVM
+{
+    public WatchVM[] WatchVMs { get; set; } = null!;
+    public class WatchVM
+    {
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required decimal Price { get; set; }
+        public required string ReferenceNumber { get; set; }
+    }
+}
