fe12dc1fd9d899efd6f09f4b21bb42b7b78ea67b
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-05-23 11:06:41 +0200

    Enhance layout and update dependencies
    - Added a new script section in `_Layout.cshtml` for additional JavaScript files.
    - Refactored the form in `Create.cshtml` for improved readability and maintainability.
    - Updated the `CreateVM` class to use the `required` keyword for property definitions.
    - Modified `WhatchApp.Web.csproj` to include a new folder for scripts in `wwwRoot`.
    - Introduced a `libman.json` file to manage client-side library dependencies for jQuery.
    - Updated `jquery.validate.min.js` and `jquery.validate.unobtrusive.min.js` to the latest versions.
    - Upgraded `jquery.min.js` to the latest version for improved features and fixes.

--- WhatchApp.Web/Views/Watches/CreateVM.cs
+++ WhatchApp.Web/Views/Watches/CreateVM.cs
@@ -6,18 +6,18 @@ namespace WhatchApp.Web.Views.Watches
     {
 
         [Required(ErrorMessage = "You must specify a name")]
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [Required(ErrorMessage = "You must specify a description")]
-        public string Description { get; set; }
+        public required string Description { get; set; }
 
         [Required(ErrorMessage = "You must specify a price")]
         [DisplayFormat(DataFormatString = "{0:N2} €")]
         [Display(Name = "Price €")]
-        public decimal Price { get; set; }
+        public required decimal Price { get; set; }
 
         [Display(Name = "Ref")]
         [Required(ErrorMessage = "You must specify a Reference number")]
-        public string ReferenceNumber { get; set; }
+        public required string ReferenceNumber { get; set; }
     }
 }
